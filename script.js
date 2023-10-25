9d301b404e9284be8d212c25139cc08d69075b19
Mdimam0786 <142109698+Mdimam0786@users.noreply.github.com>
2023-10-26 00:28:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,18 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	if (typeof sampleObject === 'object' && sampleObject !== null) {
+    return key in sampleObject;
+  }
+  return false;
 }
+}
+const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
+
+// Examples
+console.log(hasKey("red"));  
+console.log(hasKey("blue"));
+console.log(hasKey("white")); 
 
 // Do not change the code below
 const key = prompt("Enter Key.");
